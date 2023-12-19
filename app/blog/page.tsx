34fae33ea4c31bf8e42e069d42ced5c5a2c73c7c
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-19 21:10:52 +0200

    add services

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,28 +1,33 @@
+"use client";
+import { getPosts } from "@/services/getPosts";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-async function fetchData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: {
-      revalidate: 60,
-    },
-  });
-  if (!response.ok) throw new Error("Unable to fetch posts!");
-  return response.json();
-}
+export default function Blog() {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default async function Blog() {
-  const posts = await fetchData();
+  useEffect(() => {
+    setIsLoading(true);
+    getPosts()
+      .then(setPosts)
+      .finally(() => setIsLoading(false));
+  }, []);
 
   return (
     <>
       <h1 className="title">Blog</h1>
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <ul>
+          {posts.map((post: any) => (
+            <li key={post.id}>
+              <Link href={`/blog/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
---
+++ services/getPosts.ts
@@ -0,0 +1,11 @@
+export const getPosts = async () => {
+    const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  return response.json();  
+}
