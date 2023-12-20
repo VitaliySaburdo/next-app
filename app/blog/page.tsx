564333361653fb7654e994b162a75cf262d93276
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-20 16:01:37 +0200

    zustand finish

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,29 +1,13 @@
 "use client";
 import { PostSearch } from "@/components/PostSearch";
 import { Posts } from "@/components/Posts";
-import { usePosts } from "@/store";
-import { useEffect, useState } from "react";
-import { shallow } from "zustand/shallow";
 
 export default function Blog() {
-  const [posts, loading, getPosts] = usePosts(
-    (state) => [state.posts, state.loading, state.getPosts],
-    shallow
-  );
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
   return (
     <>
       <h1 className="title">Blog</h1>
       <PostSearch />
-      {loading ? (
-        <h3 className="title">Loading...</h3>
-      ) : (
-        <Posts posts={posts} />
-      )}
+      <Posts />
     </>
   );
 }
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,15 +1,28 @@
+'use client'
+import { usePosts } from "@/store";
 import Link from "next/link";
+import { useEffect } from "react";
+import { shallow } from "zustand/shallow";
 
-export const Posts = ({ posts }: any) => {
-  return (
-    <>
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
+export const Posts = () => {
+  const [posts, loading, getPosts] = usePosts(
+    (state) => [state.posts, state.loading, state.getPosts],
+    shallow
   );
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return loading ? (
+        <h3 className="title">Loading...</h3>
+      ) : (
+        <ul>
+          {posts.map((post: any) => (
+            <li key={post.id}>
+              <Link href={`/blog/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )
 };
