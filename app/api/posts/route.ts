2dfdb7d6cdab84ee35b9e3e435c20b5d82e8b489
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-21 15:30:21 +0200

    fix

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,21 +3,19 @@ import { posts } from "./posts";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-
   const query = await searchParams.get("q");
 
   let currentPosts = posts;
 
   if (query)
     currentPosts = posts.filter(
-      (post) => post.title.toLowerCase().includes(query)
+      (post) => post.title.toLowerCase().includes(query.toLowerCase())
     );
 
   return NextResponse.json(currentPosts);
-}
+};
 
 export async function POST(req: Request) {
   const body = await req.json();
-  console.log(body);
   return NextResponse.json({ body });
-}
+};
